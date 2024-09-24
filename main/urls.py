403ade661d9b8d2c93b17a7f2de50118492b8d07
lantry-glitch <allan.kwek.18@gmail.com>
2024-09-24 19:38:59 +0700

    upload tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_park_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_all_entry, delete_last_entry
+from main.views import show_main, create_park_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_all_entry, delete_last_entry, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -12,4 +12,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,29 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect
 from main.forms import ParKingEntryForm
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def show_main(request):
-    park_entries = Product.objects.all()
+    park_entries = Product.objects.filter(user=request.user)
 
     context = {
         'npm' : '2306152134',
-        'name': 'Allan Kwek',
+        'name': request.user.username,
         'class': 'PBP F',
         'aplikasi': 'ParKing',
         'slogan': 'Rule Your Parking Experience',
         'deskripsi': 'aplikasi review dan live-update kondisi ketersediaan tempat parkir beserta harganya',
-        'park_entries': park_entries
+        'park_entries': park_entries,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -23,7 +32,9 @@ def create_park_entry(request):
     form = ParKingEntryForm(request.POST or None, request.FILES)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        park_entry = form.save(commit=False)
+        park_entry.user = request.user
+        park_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -58,3 +69,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
